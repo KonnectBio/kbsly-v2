3f3d8ecf16f8cfe32d9e2fcd95b10714989920d3
kbroshan <roshaanalishah97@gmail.com>
2023-07-26 16:42:42 +0500

    test

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    loader:'akamai',
+    path: 'https://noop/',
+  }
+}
+
+module.exports = nextConfig
